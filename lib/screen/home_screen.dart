b1fe71502b51b0a326f4799f327d7d18749a6736
naturalitchy <nsubway635@gmail.com>
2023-02-20 14:36:25 +0900

    Finish web app first application.

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,13 +1,61 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+// web view plugin import
+import 'package:webview_flutter/webview_flutter.dart';
+
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+
+  WebViewController? controller;
+  HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Hello Screen Application!'),
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Text('Code Factory!!'),
+        centerTitle: true,
+
+        actions: [
+          IconButton(
+              onPressed: () {
+                if(controller != null) {
+                  controller!.loadUrl('https://blog.codefactory.ai');
+                }
+              },
+              icon: Icon(
+                Icons.home,
+              ),
+          ),
+          IconButton(
+              onPressed: () {
+                controller!.goBack();
+              },
+              icon: Icon(
+                Icons.arrow_back,
+              ),
+          ),
+          IconButton(
+              onPressed: () {
+                controller!.goForward();
+              },
+              icon: Icon(
+                Icons.arrow_forward,
+              ),
+          ),
+
+        ],
+      ),
+      body: WebView(
+        // onWebViewCreated : WebView가 생성되면 자동으로 실행.
+        onWebViewCreated: (WebViewController controller) {
+          this.controller = controller;
+        },
+
+
+        initialUrl: 'https://blog.codefactory.ai',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
     );
   }
 
